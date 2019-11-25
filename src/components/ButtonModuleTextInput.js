4fa2c9a046c892af5d1cd2a1586f0a42ba414083
Daniel Sinn <daniel.p.sinn@gmail.com>
2019-11-25 10:37:41 -0500

    Refactor ButtonModuleColourInput to extend ButtonModuleTextInput

--- src/components/ButtonModuleTextInput.js
+++ src/components/ButtonModuleTextInput.js
@@ -2,27 +2,31 @@ import PropTypes from 'prop-types';
 import React, {Component} from "react";
 
 export default class ButtonModuleTextInput extends Component {
+  getLabelContent() {
+    return this.props.value;
+  }
+
   render() {
     return (
       <label>
         <input
-          checked={this.props.stateText === this.props.text}
+          checked={this.props.stateValue === this.props.value}
           onChange={this.props.onChange}
           type="radio"
-          value={this.props.text}
+          value={this.props.value}
         />
-        {this.props.text}
+        {this.getLabelContent()}
       </label>
     )
   }
 
   shouldComponentUpdate(nextProps) {
-    return this.props.stateText !== nextProps.stateText;
+    return this.props.stateValue !== nextProps.stateValue;
   }
 }
 
 ButtonModuleTextInput.propTypes = {
   onChange: PropTypes.func,
-  stateText: PropTypes.string,
-  text: PropTypes.string
+  stateValue: PropTypes.string,
+  value: PropTypes.string
 };
